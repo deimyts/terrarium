aaadc4fa59e8ac6f1dc44dbcc87f98c02380a3cb
Cameron Jones <deimyts@gmail.com>
2018-02-17 15:45:43 -0600

    refactor(map): clean up tile array creation

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ class Map {
   constructor(width = 100, height = 100) {
     this.width = width;
     this.height = height;
-    this.tiles = [];
+    this.tiles = this.createTiles();
+  }
 
-    for (let i = 0; i < this.width * this.height; i += 1) {
-      this.tiles.push(i);
+  createTiles() {
+    const tiles = [];
+    const area = this.width * this.height;
+    for (let i = 0; i < area; i += 1) {
+      tiles.push(i);
     }
+    return tiles;
   }
 }
 
