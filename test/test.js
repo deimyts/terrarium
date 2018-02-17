71ac6e2b8c8ea80294a5973c12d372b374596688
Cameron Jones <deimyts@gmail.com>
2018-02-17 15:37:01 -0600

    refactor: cleanup tests

--- test/test.js
+++ test/test.js
@@ -2,49 +2,57 @@ import test from 'ava';
 
 import { Game, Map } from '../src/main';
 
+function createGame() {
+  return new Game();
+}
+
+function createMap(width, height) {
+  return new Map(width, height);
+}
+
 test('It should create a game object', (t) => {
-  const world = new Game();
-  t.not(world, undefined);
+  const game = createGame();
+  t.not(game, undefined);
 });
 
 test('The game shoud have a seed property', (t) => {
-  const world = new Game();
-  t.not(world.seed, undefined);
+  const game = createGame();
+  t.not(game.seed, undefined);
 });
 
-test('The world should have a map', (t) => {
-  const world = new Game();
-  t.not(world.map, undefined);
+test('The game should have a map', (t) => {
+  const game = createGame();
+  t.not(game.map, undefined);
 });
 
 test('The map should have a width', (t) => {
-  const map = new Map();
+  const map = createMap();
   t.not(map.width, undefined);
 });
 
 test('The map should have a height', (t) => {
-  const map = new Map();
+  const map = createMap();
   t.not(map.width, undefined);
 });
 
 test('You should be able to set the map width and height when you create it', (t) => {
-  const map = new Map(50, 50);
+  const map = createMap(50, 50);
   t.is(map.width, 50);
   t.is(map.height, 50);
 });
 
 test('The map should have tiles', (t) => {
-  const map = new Map(2, 2);
+  const map = createMap(2, 2);
   t.true(map.tiles instanceof Array);
 });
 
 test('The map should have a number of tiles equal to it\'s area', (t) => {
-  const map1 = new Map(2, 2);
+  const map1 = createMap(2, 2);
   t.is(map1.tiles.length, 4);
 
-  const map2 = new Map(3, 3);
+  const map2 = createMap(3, 3);
   t.is(map2.tiles.length, 9);
 
-  const map3 = new Map(2, 3);
+  const map3 = createMap(2, 3);
   t.is(map3.tiles.length, 6);
 });
