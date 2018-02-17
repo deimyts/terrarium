648e8ddeae7d2d48e114cc44dce15ff55997d397
Cameron Jones <deimyts@gmail.com>
2018-02-17 14:37:00 -0600

    refactor: remove extra test

--- test/test.js
+++ test/test.js
@@ -2,10 +2,6 @@ import test from 'ava';
 
 import Game from '../src/main';
 
-test('the module should exist', (t) => {
-  t.not(Game, undefined);
-});
-
 test('it should create a game object', (t) => {
   const world = new Game();
   t.not(world, undefined);
