9ea069f321e64a3257cbb32d415bb87bb1b2f189
Cameron Jones <deimyts@gmail.com>
2018-02-17 21:46:26 -0600

    refactor: put Map class in its own file

---
+++ src/map.js
@@ -0,0 +1,51 @@
+const seedrandom = require('seedrandom');
+
+class Map {
+  constructor(width = 100, height = 100) {
+    this.width = width;
+    this.height = height;
+    this.tiles = this.createTiles();
+  }
+
+  createTiles() {
+    const grid = Map.createGrid(this.width, this.height);
+    return Map.fillGridWithValues(grid);
+  }
+
+  static createGrid(width, height) {
+    const xAxis = this.createIndependentAxis(width);
+    return this.createDependentAxis(height, xAxis);
+  }
+
+  static createIndependentAxis(length) {
+    return this.createFilledArray([], length);
+  }
+
+  static createDependentAxis(length, xAxis) {
+    return xAxis.map(() => this.createFilledArray(null, length));
+  }
+
+  static fillGridWithValues(grid) {
+    return grid.map(column => this.fillColumnWithValues(grid, column));
+  }
+
+  static fillColumnWithValues(grid, column) {
+    const xVal = grid.indexOf(column);
+    return column.map((tile, yVal) => this.formatCoordinates(xVal, yVal));
+  }
+
+  static formatCoordinates(x, y) {
+    return { x, y, value: this.assignRandomValuetoTile() };
+  }
+
+  static createFilledArray(fillValue, length) {
+    return new Array(length).fill(fillValue);
+  }
+
+  static assignRandomValuetoTile(seed) {
+    const val = seedrandom(seed);
+    return val();
+  }
+}
+
+export default Map;
